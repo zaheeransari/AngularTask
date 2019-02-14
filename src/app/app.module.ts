84faa9a64561e89e474a17924ed859a4f5769822
zaheer <zaheeransari.in@gmail.com>
2019-02-14 12:28:34 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Routes, RouterModule } from '@angular/router';
 import { UserListComponent } from './user-list/user-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ModalModule } from 'ngx-bootstrap';
 import { RestService } from './services/rest.service';
 import { ShowJsonComponent } from './show-json/show-json.component';
-import { FilterdataPipe } from './filterdata.pipe';
+import { FilterdataPipe } from './pipe/filterdata.pipe';
 import { FormsModule } from '@angular/forms';
 import { MasterMenuComponent } from './master-menu/master-menu.component';
 
